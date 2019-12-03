f96d1eef494b1686f8fe7a4bcaa317774ac6df0b
amtello <ytemilouwa@yahoo.com>
2019-12-03 22:51:58 +0100

    Initial Commit

---
+++ loadingAntsAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loadingAntsAnimation
+//
+//  Created by Tello on 03/12/2019.
+//  Copyright © 2019 Tello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
